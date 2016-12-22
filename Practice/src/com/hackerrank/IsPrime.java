8b806674c763cb431b001e2088c0c2eef55e51ee
Bakzee <jibuchand11@gmail.com>
2016-12-22 10:07:23 +0545

    Added is prime with math.sqrt(n) complexity

---
+++ Practice/src/com/hackerrank/IsPrime.java
@@ -0,0 +1,16 @@
+package com.hackerrank;
+
+public class IsPrime {
+	public static boolean checkIsPrime(int n){
+	for(int i=2;i<Math.sqrt(n);i++)
+        if(n%i==0) return false;
+    return true;
+	}
+	public static void main(String[] args){
+		Boolean val1 = checkIsPrime(12);
+		assert val1.toString().equals("false");
+		Boolean val2 = checkIsPrime(13);
+//		assert val2.toString().equals("false");
+	}
+
+}
