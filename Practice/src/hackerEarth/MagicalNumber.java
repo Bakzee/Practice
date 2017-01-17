5adb4b08b8c9a78201a47741f086346fa16cfc24
Bakzee <jibuchand11@gmail.com>
2017-01-17 18:26:23 +0530

    MagicalNUmber:ascii val>=127 val 127 and val<65 val=67 passes all cases sucessfully

--- Practice/src/hackerEarth/MagicalNumber.java
+++ Practice/src/hackerEarth/MagicalNumber.java
@@ -3,7 +3,7 @@ package hackerEarth;
 public class MagicalNumber {
 	public static boolean isPrime(int val){
         if((val%2)==0) return false;
-        for(int i=3;i<val;i++){
+        for(int i=3;i<val;i+=2){
             if((val%i)==0) return false;
         }
         return true;
@@ -14,18 +14,23 @@ public class MagicalNumber {
 			int ascii1=ascii;
 			int ascii2=ascii;
 			while(isPrime(ascii1)==false) {
-				ascii1++;
+				++ascii1;
+				if(ascii1>=127) ascii1=113;
 			}
 			while(isPrime(ascii2)==false) {
-				ascii2--;
+				--ascii2;
+				//if(ascii2<67) ascii2=67;
 			}
-			if((ascii1-ascii)<(ascii-ascii2)) System.out.print((char)ascii1);
-			else System.out.print((char)ascii2);
+			if(ascii2<=67) ascii2=67;
+			if((ascii-ascii2)>(ascii1-ascii))
+				System.out.print((char)ascii1);
+			else 
+				System.out.print((char)ascii2);
 		}
 	}
 
 	public static void main(String[] args) {
-		String str="AFREEN";
+		String str="kCeYamOYCSOgOCaYCeaa";
 		checkMagicalString(str);
 	}
 
